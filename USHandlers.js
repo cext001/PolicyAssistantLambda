9919abae3824fac8e7321a21b09e5f25438367d9
cext001 <cext001.hexaware@gmail.com>
2018-04-06 17:41:53 +0530

    added latest changes

--- USHandlers.js
+++ USHandlers.js
@@ -1,5 +1,8 @@
 const helper = require('./helper');
 
+var claimId;
+var claimStatusIntentInvoked = false;
+
 module.exports = {
     'LaunchRequest': function () {
         this.emit(':ask', this.t('HELLO_MESSAGE'), this.t('HELP_MESSAGE'));
@@ -19,28 +22,48 @@ module.exports = {
         this.response.speak(this.t('STOP_MESSAGE'));
         this.emit(':responseReady');
     },
-    "GetClaimStatusIntent": function () {
+    "claimStatusIntent": function () {
         var speechOutput;
-        var claimId;
+        claimStatusIntentInvoked = true;
         if (this.event.request.intent.slots.claimId.value) {
             claimId = this.event.request.intent.slots.claimId.value;
             console.log('claimId:' + claimId);
-            return helper.getClaimStatus(claimId).then((result) => {
-                speechOutput = result;
-                this.response.speak(speechOutput).shouldEndSession(false);
-                this.emit(':responseReady');
-            }).catch((err) => {
-                speechOutput = err;
-                this.response.speak(speechOutput).shouldEndSession(false);
-                this.emit(':responseReady');
-            });
+            if (claimId.length !== 11) {
+                console.log('INVALID_CLAIM_ID');
+                speechOutput = this.t('INVALID_CLAIM_ID');
+            } else {                
+                return helper.getClaimStatus(claimId).then((result) => {
+                    speechOutput = result;
+                }).catch((err) => {
+                    speechOutput = err;
+                });
+                console.log('API CALL SUCCESS: ' + speechOutput);
+            }
         } else {
-            speechOutput = "<s>Please provide the claim number.</s>";
-            this.response.speak(speechOutput).shouldEndSession(false);
-            this.emit(':responseReady');
+            console.log('CLAIM_ID_NEEDED');
+            speechOutput = this.t('CLAIM_ID_NEEDED');
+        }
+        this.response.speak(speechOutput).shouldEndSession(false);
+        this.emit(':responseReady');
+    },
+    "claimIdIntent": function () {
+        var speechOutput;
+        claimId = this.event.request.intent.slots.claimId.value;
+        if (claimId.length === 11) {
+            if (claimStatusIntentInvoked) {
+                return helper.getClaimStatus(claimId).then((result) => {
+                    speechOutput = result;
+                }).catch((err) => {
+                    speechOutput = err;
+                });
+                console.log('API CALL SUCCESS: ' + speechOutput);
+            }
+        } else {
+            console.log('INVALID_CLAIM_ID');
+            speechOutput = this.t('INVALID_CLAIM_ID');
         }
     },
-    "GetRepairPaymentStatusIntent": function () {
+    "repairPaymentIntent": function () {
         var speechOutput;
         var claimId;
         if (this.event.request.intent.slots.claimId.value) {
@@ -50,12 +73,9 @@ module.exports = {
                 speechOutput = res;
             });
         } else {
-            speechOutput = "Please provide the claim number.";
+            speechOutput = this.t('CLAIM_ID_NEEDED');
         }
         this.response.speak(speechOutput).shouldEndSession(false);
         this.emit(':responseReady');
-    },
-    "DUGetClaimStatusIntent": function () {
-        this.emit(':ask', this.t('HELLO_MESSAGE'));
     }
 };
--- language.js
+++ language.js
@@ -4,7 +4,9 @@ module.exports = {
             'HELLO_MESSAGE': '<s>Welcome to the policy assistant.</s>',
             'HELP_MESSAGE': '<s>You can ask a question like, what is my claim status.</s>',
             'HELP_REPROMPT': '<s>What can I help you with?</s>',
-            'STOP_MESSAGE': '<s>Happy to help you. Goodbye!</s>'
+            'STOP_MESSAGE': '<s>Happy to help you. Goodbye!</s>',
+            'CLAIM_ID_NEEDED': '<s>Can you please provide the claim ID.</s>',
+            'INVALID_CLAIM_ID': '<s>Can you please provide the claim ID.</s>'
         }
     },
     'de-DE': {
@@ -12,7 +14,9 @@ module.exports = {
             'HELLO_MESSAGE': '<s>Willkommen beim Politikassistenten.</s>',
             'HELP_MESSAGE': '<s>Sie können eine Frage stellen, was ist mein Anspruchsstatus.</s>',
             'HELP_REPROMPT': '<s>Womit kann ich dir helfen?</s>',
-            'STOP_MESSAGE': '<s>Gerne, um dir zu helfen. Auf Wiedersehen!</s>'
+            'STOP_MESSAGE': '<s>Gerne, um dir zu helfen. Auf Wiedersehen!</s>',
+            'CLAIM_ID_NEEDED': '<s>Können Sie bitte die Anspruchs-ID angeben?.</s>',
+            'INVALID_CLAIM_ID': '<s>Können Sie bitte die Anspruchs-ID angeben?.</s>'
         }
     }
 };
