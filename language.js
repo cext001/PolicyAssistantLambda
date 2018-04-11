eb89504e5b7396831a4f44abe194c6706f7420b3
cext001 <cext001.hexaware@gmail.com>
2018-04-11 15:48:39 +0530

    messages corrected

--- language.js
+++ language.js
@@ -1,12 +1,12 @@
 module.exports = {
     'en-US': {
         'translation': {
-            'HELLO_MESSAGE': '<s>Welcome to the policy assistant.</s>',
+            'HELLO_MESSAGE': '<s>Hi</s>Welcome to the policy assistant.</s><s>What can I do for you.</s>',
             'HELP_MESSAGE': '<s>You can ask a question like, what is my claim status.</s>',
             'HELP_REPROMPT': '<s>What can I help you with?</s>',
             'STOP_MESSAGE': '<s>Happy to help you. Goodbye!</s>',
-            'CLAIM_ID_NEEDED': '<s>Can you please provide the claim ID.</s>',
-            'INVALID_CLAIM_ID': '<s>Can you please provide the claim ID.</s>'
+            'CLAIM_ID_NEEDED': '<s>Can you please provide the claim number.</s>',
+            'INVALID_CLAIM_ID': '<s>Can you please provide the claim number.</s>'
         }
     },
     'de-DE': {
